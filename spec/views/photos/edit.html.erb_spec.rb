de8372868636283c024d36a212c6904501951d69
Simone <simonecaplin@gmail.com>
2018-07-07 13:59:54 +0100

    Generate scaffold for photos

---
+++ spec/views/photos/edit.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "photos/edit", type: :view do
+  before(:each) do
+    @photo = assign(:photo, Photo.create!(
+      :caption => "MyText"
+    ))
+  end
+
+  it "renders the edit photo form" do
+    render
+
+    assert_select "form[action=?][method=?]", photo_path(@photo), "post" do
+
+      assert_select "textarea[name=?]", "photo[caption]"
+    end
+  end
+end
