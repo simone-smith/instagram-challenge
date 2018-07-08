55baf697fef6920d243bbd293c4f116744788d55
Simone <simonecaplin@gmail.com>
2018-07-08 17:15:24 +0100

    Users can log in and out, and must be authenticated to post/view photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class PhotosController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_photo, only: [:show, :edit, :update, :destroy]
 
   # GET /photos
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    user_name "Simone"
+    email "simone@email.com"
+    password "mypassword"
+    id 1
   end
 end
